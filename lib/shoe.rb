8dab5f5f8aa687217733085171622c4a8b1376d6
AlyssaEdwards13 <edwardsalyssa83@gmail.com>
2018-06-08 16:50:01 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,31 @@
-# Make your shoe class here!
+class Shoes
+  def color=(color_base)
+  @this_shoes_color = color_base 
+ end 
+
+def color 
+  @this_shoes_color
+end 
+  
+  def swoosh=(swoosh_color)
+    @this_shoes_swoosh = swoosh_color
+  end 
+  
+  def swoosh 
+    @this_shoes_swoosh
+end 
+
+  def laces=(laces_color)
+    @this_shoes_laces = laces_color
+end
+
+def laces 
+  @this_shoes_laces
+end 
+end 
+
+sams_shoe = Shoes.new 
+sams_shoe.color = "red"
+sams_shoe.swoosh = "white"
+sams_shoe.laces = "white"
+puts "The color of Sam's shoe is #{sams_shoe.color}, the swoosh is #{sams_shoe.swoosh}, the laces are #{sams_shoe.laces}"
